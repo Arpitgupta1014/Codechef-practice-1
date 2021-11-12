96dbdb77fb378ced1844b809577203d6e7837317
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2021-11-12 18:44:17 +0530

    Adding code

---
+++ VOWELTB.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	char c;
+	cin>>c;
+    if(c=='A' || c=='E' || c=='I'|| c=='O' || c=='U'){
+        cout<<"Vowel\n";
+    }
+    else{
+        cout<<"Consonant\n";
+    }
+	return 0;
+}
